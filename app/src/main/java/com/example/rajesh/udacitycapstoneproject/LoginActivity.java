0bc07f2030266c7f4730b25427e3264497f3ae55
rajesh <rajeshkhadka@lftechnology.com>
2016-06-29 14:11:05 +0545

    signup: add sign up view

--- app/src/main/java/com/example/rajesh/udacitycapstoneproject/LoginActivity.java
+++ app/src/main/java/com/example/rajesh/udacitycapstoneproject/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.example.rajesh.udacitycapstoneproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @Bind(R.id.username)
+    EditText username;
+    @Bind(R.id.password)
+    EditText password;
+    @Bind(R.id.login)
+    Button login;
+    @Bind(R.id.sign_up)
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.sign_up)
+    public void onClick() {
+        startActivity(new Intent(this,SignUpActivity.class));
     }
 }
